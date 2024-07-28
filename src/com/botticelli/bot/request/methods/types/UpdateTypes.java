26c057e1c7598c7f51045ab02491569b56d166ab
Jaeger <rosati.1595834@gmail.com>
2024-07-29 00:00:39 +0200

    UpdateTypes Refactor

--- src/com/botticelli/bot/request/methods/types/UpdateTypes.java
+++ src/com/botticelli/bot/request/methods/types/UpdateTypes.java
@@ -1,6 +1,6 @@
 package com.botticelli.bot.request.methods.types;
 
-public enum updateTypes {
+public enum UpdateTypes {
 	message,edited_channel_post,callback_query,chosen_inline_result,inline_query,
 	edited_message,channel_post, shipping_query, pre_checkout_query
 }
